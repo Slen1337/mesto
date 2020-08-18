7c6ecfa92e124abfdaef593e27706c3b3a389e38
Dmitry Nikonov <saylen211@gmail.com>
2020-08-18 17:25:52 +0300

    webpack created

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+   entry: './src/pages/index.js',
+   mode: 'production',
+   output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+  rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+            MiniCssExtractPlugin.loader,
+                { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      }, 
+     {
+        test: /\.(png|jpe?g|gif|woff|woff2|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }, 
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+},
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+ };
